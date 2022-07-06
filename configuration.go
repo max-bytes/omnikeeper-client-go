9595b6c7e07f6c5102ab4799fe38266c234d6907
generator <generator@max-bytes.com>
2022-07-06 15:39:09 +0000

    Update to version 19.0.0-rc45

--- configuration.go
+++ configuration.go
@@ -101,7 +101,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/19.0.0-rc44/go",
+		UserAgent:        "OpenAPI-Generator/19.0.0-rc45/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
